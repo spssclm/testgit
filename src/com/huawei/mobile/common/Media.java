dc3a4466148e48d48ab68211afb193430afb3ec8
linfeng.yuan <linfeng.yuan@cloudcross.com>
2016-10-17 18:35:59 +0800

    Linfeng add Media.java

---
+++ src/com/huawei/mobile/common/Media.java
@@ -0,0 +1,27 @@
+package com.huawei.mobile.common;
+
+public class Media {
+
+	String VIDEO = "1";
+	String BANNER = "0";
+	// wang add
+	String TYPE = "1";
+	String Type = "2";
+	
+	//xiaoyu add
+	String XIAOYU = "4";
+	//xiaoyu add2
+	String XIAOYU2 = "42";
+	//xiaoyu add3
+	String XIAOYU3 = "43";
+	
+	
+	//yuan add
+	String LINFENG = "LINFENG";
+	String LINFENG2 = "LINFENG2";
+	String LINFENG3 = "LINFENG3";
+	String LF4 = "LF4";
+	String LF5 = "LF5";
+	String LF6 = "LF6";
+	String LINFENG7 = "LINFENG7";
+}
